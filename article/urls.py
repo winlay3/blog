71dd016d70a69895c9f079fbe74d89208271c681
winlay3 <winmyintkyaw1234@gmail.com>
2024-11-03 23:30:17 +0700

    remove

--- article/urls.py
+++ article/urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
     path('join/signup', views.signup, name = 'signup'),
     path('join/login', views.login,name='login'),
     path('join/logout',views.logout, name = 'logout'),
-    path('manage/blog/create',views.create_article , name="create_article")
+    path('dashboard',views.dashboard,name="dashboard"),
+    path('manage/blog/create',views.create_article , name="create_article"),
+    path('manage/blog/edit/<int:id>',views.edit_article,name="edit_article"),
+    path('manage/blog/remove/<int:id>',views.remove_article,name="remove_article"),
+    path('manage/blog/handlepublishing',views.handle_publishing,name="handle_publishing")
 ]
--- article/views.py
+++ article/views.py
@@ -1,12 +1,14 @@
+import json
 from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 from django.shortcuts import render,redirect,get_object_or_404
 from django.db import IntegrityError
 from django.contrib import messages
+from django.http import HttpResponse,JsonResponse
 from . models import Author,Article,ArticleForm
 # Create your views here.
 def index(request):
     
-    articles = Article.objects.all()
+    articles = Article.objects.filter(is_publish=True)
     return render(request, "index.html", {
         'articles': articles
     })
@@ -20,6 +22,36 @@ def article(request, id):
         "article": article,
         "is_owner": is_owner
     })
+def dashboard(request):
+    articles = request.user.article.all()
+    total_article = len(articles)
+    total_publish = request.user.article.filter(is_publish = True).count()
+    
+    return render(request, "dashboard.html",{
+        "articles": articles,
+        "total_article": total_article,
+        "total_publish": total_publish
+    })
+
+def handle_publishing(request):
+    if request.method != "POST":
+        return JsonResponse({"error": "POST only route."}, status=400)
+    req_data = json.loads(request.body)
+    post_id = req_data.get('id', '')
+    article = Article.objects.filter(pk = post_id, author = request.user).first()
+    if article == None:
+        return JsonResponse({"error": "POST not found."},status=400)
+    article.is_publish = not article.is_publish
+    article.save()
+    action_publish = 'Published' if article.is_publish else 'Unpublished'
+    msg = 'Article was ' + action_publish
+    return JsonResponse({
+        "error": "",
+        "msg": msg,
+        "operation": action_publish
+    },status=200)
+    return HttpResponse(request)
+
 def create_article(request):
     if request.method == "POST":
         form = ArticleForm(request.POST,request.FILES)
@@ -36,9 +68,32 @@ def create_article(request):
     return render(request, "create_article.html",{
         "form": form
     })
+def edit_article(request,id):
+    article = get_object_or_404(Article,pk=id)
 
+    if article.author.id != request.user.id:
+        return render (request,'error.html')
+    else:
+        if request.method == "POST":
+            form = ArticleForm(request.POST,request.FILES,instance = article)
+            if form.is_valid():
+                
+                article = form.save(commit=False)
+                article.author_id = request.user.id
+                article.save()
+                messages.success(request,"Post Edited")
+                return redirect("home")
+                
+        else:
+            form = ArticleForm(instance=article)
+        return render(request,'edit_article.html', {
+            'form' : form,
+            'post_id': article.id
+        })
 
-
+def remove_article(request,id):
+    print(id)
+    return redirect('dashboard')
 
 
 def signup(request):
